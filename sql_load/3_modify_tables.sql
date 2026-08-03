61862355e4122c7cdd45dc54c00f4c7370f53fde
hweaj <minatozakimng@gmail.com>
2026-08-03 19:51:16 +0300

    commit

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,68 @@
+/* ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
+Database Load Issues (follow if receiving permission denied when running SQL code below)
+
+NOTE: If you are having issues with permissions. And you get error: 
+
+'could not open file "[your file path]\job_postings_fact.csv" for reading: Permission denied.'
+
+1. Open pgAdmin
+2. In Object Explorer (left-hand pane), navigate to `sql_course` database
+3. Right-click `sql_course` and select `PSQL Tool`
+    - This opens a terminal window to write the following code
+4. Get the absolute file path of your csv files
+    1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
+5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
+
+\copy company_dim FROM 'C:\Users\HUAWEI\Desktop\sql yuot\csv_files\company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy skills_dim FROM 'C:\Users\HUAWEI\Desktop\sql yuot\csv_files\skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy job_postings_fact FROM 'C:\Users\HUAWEI\Desktop\sql yuot\csv_files\job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+\copy skills_job_dim FROM 'C:\Users\HUAWEI\Desktop\sql yuot\csv_files\skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+*/
+
+-- NOTE: This has been updated from the video to fix issues with encoding
+
+COPY company_dim
+FROM 'company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM 'skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM 'job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM 'skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+TRUNCATE TABLE skills_job_dim CASCADE;
+TRUNCATE TABLE job_postings_fact CASCADE;
+TRUNCATE TABLE company_dim CASCADE;
+TRUNCATE TABLE skills_dim CASCADE;
+
+-- Затем выполняем COPY
+COPY company_dim
+FROM 'company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM 'skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM 'job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM 'skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+SELECT *
+FROM skills_job_dim
+limit 100
